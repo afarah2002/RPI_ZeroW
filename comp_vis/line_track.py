fbb3ab660f50ee90c393bd899d377552b731fc04
Albert Farah <arfarah2002@gmail.com>
2019-01-29 00:21:51 -0500

    first test of cv2 morpohology on RPI, exporting to another port, 8082

--- comp_vis/line_track.py
+++ comp_vis/line_track.py
@@ -1,4 +1,34 @@
 import cv2
 import numpy as np
+import socket
+import cPickle 
+from copy import deepcopy
 
 #this is the line following code for the AVE
+
+def detectLine(feed):#detects contrasting lines in feed
+	upper_thresh = 0
+	lower_thresh = 10 #contrast thresholds
+
+	k_dim = 5
+	kernel = np.ones((k_dim,k_dim),np.uint8)
+
+	cap = cv2.VideoCapture(feed) #access webcam feed from IPport#
+	while (cap.isOpened()):
+		ret, frame = cap.read()
+		img = cv2.inRange(frame,lower_thresh,upper_thresh)
+		img = cv2.morphologyEx(img,cv2.MORPH_CLOSE,kernel) #frame morphology for contrast
+		print("processing")
+
+		print("exporting image....")
+		clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		clientsocket.connect(('localhost',8082)) #connect to network
+		data = cPickle.dumps(img)
+		clientsocket.sendall(struct.pack("H", len(data))+data)
+
+# def exportImage(img): #exports image to new port 
+	
+	# while True:
+		
+
+detectLine('http://192.168.1.180:8081/')

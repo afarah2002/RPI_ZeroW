1ea8454bb7960639a65c2bfd1c5a261ae19a6204
Albert Farah <arfarah2002@gmail.com>
2019-07-04 17:44:02 -0400

    testing writing to arduino

--- comp_vis/test_angles_writer.py
+++ comp_vis/test_angles_writer.py
@@ -3,7 +3,14 @@ import numpy as np
 import serial
 
 
+
 #----------code starts here!----------#
+
+#arduino
+port = '/dev/ttyACM0'
+ard = serial.Serial(port) 
+
 angles = list(np.arange(0,180))
 for i in angles:
 	print(i)
+	ard.write(str(i))
